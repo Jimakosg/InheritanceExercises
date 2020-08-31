39b85148cb379bd81440e032ff707acd4413c86b
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:17:31 +0300

    Simple Algorithm Exercise 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Stack;
+
+class Main {
+        public static void main(String[] args) {
+            // write your code here
+
+            int number = 8; //the factorial number that we want to calculate, example: 8
+
+            int result = calculate(number);
+            System.out.println("The factorial of " + number + " is: " + result);
+
+        }
+
+        public static int calculate(int number) {
+
+            if (number >= 1) {
+
+                return number * calculate(number - 1);
+
+            } else return 1;
+
+        }
+
+
+    }
