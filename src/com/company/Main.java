f1a50acc62d1fcbbbaaa02b8d1a391b2f849e542
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:56:09 +0300

    Exercises on Arrays, Collections 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,45 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        Scanner scanner = new Scanner(System.in);
-        float num = scanner.nextFloat();
-        System.out.println(num %  1);
+        int x = 115;
+        List<Integer> list = new ArrayList<Integer>();
+        //Adding integers into the list.
+        for (int i = 1; i <= x; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+
+        //Checking whether the number is prime. If it's not, remove the item from the list.
+        //If the number is prime, then remove it from the list.
+        //Eventually ALL non-prime numbers are removed and the new list is printed again.
+        for (int i = 0; i < list.size() -1 ; i++){
+            if (!isPrime(list.get(i))){
+                list.remove(list.get(i));
+                i--;
+            }
+        }
+        System.out.println(list);
     }
+
+    //This boolean method checks whether the inserted integer in the list is a prime or not using the % sign.
+//If it returns 0, then it isn't a prime, otherwise it is.
+    public static boolean isPrime(int x) {
+        boolean flag = true;
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
 
 
 
