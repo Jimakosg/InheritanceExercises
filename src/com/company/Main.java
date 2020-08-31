344030bb9e2fea21ce1f55023e9f49b154e93f8a
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 12:01:21 +0300

    Exercises on Arrays, Collections 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        String string = "madam";
+        char[] array = string.toCharArray();
+        System.out.println(palidromeCheck(array));
 
-        System.out.println(reverse("Kalhmera Kalhspera"));
+        //System.out.println(isPalindrome("madam"));
+
 
     }
 
-    public static String reverse(String str) {
-        char[] charArr = str.toCharArray();
-        int size = charArr.length;
-        Stack stack = new Stack();
+    public static boolean palidromeCheck(char[] string){
+        int i = 0;
+        int j = string.length - 1;
 
-        for(int i = 0; i < size; ++i) {
-            stack.push(charArr[i]);
+        while (i > j){
+            if (string[i] != string[j]);
+            return false;
         }
-
-        for(int i = 0; i < size; ++i) {
-            charArr[i] = (char) stack.pop();
-        }
-
-        return String.valueOf(charArr);
+        i++;
+        j++;
+        return true;
     }
+
+    //This is a second solution.
+    public static boolean isPalindrome(String str){
+
+        return str.equals(new StringBuffer().append(str).reverse().toString());
+
+    }
+
 
 }
 
