f5f56e62c167eafd57042c39905d3d04099036ca
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:57:50 +0300

    Exercises on Arrays, Collections 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,23 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int x = 115;
-        List<Integer> list = new ArrayList<Integer>();
-        //Adding integers into the list.
-        for (int i = 1; i <= x; i++) {
-            list.add(i);
-        }
-        System.out.println(list);
+        int x = 10;
+        Set set = new HashSet();
 
-        //Checking whether the number is prime. If it's not, remove the item from the list.
-        //If the number is prime, then remove it from the list.
-        //Eventually ALL non-prime numbers are removed and the new list is printed again.
-        for (int i = 0; i < list.size() -1 ; i++){
-            if (!isPrime(list.get(i))){
-                list.remove(list.get(i));
-                i--;
-            }
+        for (int i = 0; i < x; i++){
+            //The set check whether there are duplicate values and removes them automatically.
+            //The set.add(5); is removed since the number 5 already exists in the list.
+            set.add(i);
+            set.add(5);
+
         }
-        System.out.println(list);
+        System.out.println(set);
     }
 
-    //This boolean method checks whether the inserted integer in the list is a prime or not using the % sign.
-//If it returns 0, then it isn't a prime, otherwise it is.
-    public static boolean isPrime(int x) {
-        boolean flag = true;
-        for (int i = 2; i < x; i++) {
-            if (x % i == 0) {
-                flag = false;
-            }
-        }
-        return flag;
-    }
 
 
 
