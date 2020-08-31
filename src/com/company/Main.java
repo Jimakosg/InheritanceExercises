273a687b7923416f8a74299e1701861650e7f1db
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:35:25 +0300

    Simple Algorithm Exercise 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,26 +2,15 @@ package com.company;
 
 import java.util.Stack;
 
-class Main {
-        public static void main(String[] args) {
-            // write your code here
+public class Main {
 
-            int number = 8; //the factorial number that we want to calculate, example: 8
+    public static void main(String[] args) {
+        // write your code here
+        int max = Integer.MAX_VALUE;
 
-            int result = calculate(number);
-            System.out.println("The factorial of " + number + " is: " + result);
-
-        }
-
-        public static int calculate(int number) {
-
-            if (number >= 1) {
-
-                return number * calculate(number - 1);
-
-            } else return 1;
-
-        }
-
+        System.out.println(max);
 
     }
+
+}
+
