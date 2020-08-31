594f8d71fe00dc2a5a90410256b3852ba6729013
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:54:22 +0300

    Simple Algorithm Exercise 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        double number = 1.6666;
-        //The -1 exists because the system counts the "." as a digit but the exercise only asks for Digits
-        long length = String.valueOf(number).length() - 1;
-
-        System.out.println(number);
-        System.out.println(length);
+        Scanner scanner = new Scanner(System.in);
+        float num = scanner.nextFloat();
+        System.out.println(num %  1);
     }
+
 
 
 }
