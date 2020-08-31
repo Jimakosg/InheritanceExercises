e2654bdf13d40dbe008ea13a807d2f21fec121be
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:53:11 +0300

    Simple Algorithm Exercise 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int number = 17; //17 is a prime number.
-        boolean flag = false;
+        int number = 3;
 
-        for (int i = 2; i <= number / 2; i++){
-            if(number % i == 0){
-                flag = true;
-                break;
-            }
-        }
+        System.out.println(calculate(number));
 
-        if(!flag){
-            System.out.println("The number " + number + " is a prime");
-
-        }else System.out.println("The number " + number + " is not a prime");
 
     }
+
+    static double calculate(int number)
+    {
+        double sum = 0;
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+
+            factorial *= i;
+
+            sum += 1.0/factorial;
+        }
+        return sum;
+    }
+
 }
 
 
