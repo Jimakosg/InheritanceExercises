6a70fa142539d731a7f9432d006acb65b774c188
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 17:17:15 +0300

    String Class - Regex Exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,29 @@ package com.company;
 import java.util.*;
 
 public class Main {
+        public static void main(String[] args) {
+            String password = "aaZZa44@";
+            String regex = "(?i)(?:([a-z0-9])\\\\1{2,})*(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+            System.out.println(password.matches(regex));
 
-    public static void main(String[] args) {
-        String string = "madam";
-        char[] array = string.toCharArray();
-        System.out.println(palidromeCheck(array));
-
-        //System.out.println(isPalindrome("madam"));
+            String okPwd = "(?i)(?:([a-z0-9])\\\\1{2,})*(?=.*[0-9])";
+            String strongPwd = "(?i)(?:([a-z0-9])\\\\1{2,})*.{8,}";
+            String invalidPwd = ".{8,}";
 
 
-    }
+            if(password.matches(okPwd)){
+                System.out.println("Password Ok");
+            }else if (password.matches(regex)){
+                System.out.println("Very Strong Password");
+            }else if (password.matches(strongPwd)){
+                System.out.println("Strong Password");
+            }else if (!password.matches(regex) || !password.matches(invalidPwd)){
+                System.out.println("Invalid Password. Your password needs to be at least 8 letters long, " +
+                        "to contain at least one uppercase letter, one lowercase letter, " +
+                        "a digit and a special character");
 
-    public static boolean palidromeCheck(char[] string){
-        int i = 0;
-        int j = string.length - 1;
-
-        while (i > j){
-            if (string[i] != string[j]);
-            return false;
+            }
         }
-        i++;
-        j++;
-        return true;
-    }
-
-    //This is a second solution.
-    public static boolean isPalindrome(String str){
-
-        return str.equals(new StringBuffer().append(str).reverse().toString());
-
-    }
-
 
 }
 
