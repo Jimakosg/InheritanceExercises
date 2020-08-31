48d851ed734d3eaa7c1959b51ff468ea195cb5db
Jimakosg <dimitrisgrig@hotmail.com>
2020-08-31 11:52:29 +0300

    Simple Algorithm Exercise 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int max = Integer.MAX_VALUE;
+        int number = 17; //17 is a prime number.
+        boolean flag = false;
 
-        System.out.println(max);
+        for (int i = 2; i <= number / 2; i++){
+            if(number % i == 0){
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag){
+            System.out.println("The number " + number + " is a prime");
+
+        }else System.out.println("The number " + number + " is not a prime");
 
     }
-
 }
+
 
